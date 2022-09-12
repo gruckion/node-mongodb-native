2abeb1497087c4e9a5c5b910351298cc77a325fb
Durran Jordan <durran@gmail.com>
2022-09-12 20:45:08 +0200

    test(NODE-4234): refresh aws credentials

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -601,12 +601,7 @@ for (const version of ['5.0', 'rapid', 'latest']) {
             CSFLE_GIT_REF: ref
           }
         },
-        {
-          func: 'run custom csfle tests',
-          vars: {
-            CSFLE_GIT_REF: ref
-          }
-        }
+        { func: 'run custom csfle tests' }
       ]
     });
 
@@ -642,7 +637,7 @@ for (const version of ['5.0', 'rapid', 'latest']) {
           {
             func: 'run custom csfle tests',
             vars: {
-              CSFLE_GIT_REF: ref
+              REFRESH_AWS_CREDENTIALS: true
             }
           }
         ]
